0b390259e3b363746a351387a229cbbb3f10f1d1
Imran Ismail <imran.codely@gmail.com>
2016-08-22 20:45:54 +0800

    Update dockerfile and run script

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,5 @@ FROM 127labs/elixir:latest
 RUN curl -sL https://deb.nodesource.com/setup_6.x | bash -
 RUN apt-get install -y nodejs
 
-COPY ./run.sh /
-ENTRYPOINT ["/run.sh"]
+COPY ./run.sh /tmp
+CMD ["/tmp/run.sh"]
--- run.sh
+++ run.sh
@@ -1,24 +1,18 @@
+#!/bin/bash
+
 MIX_ENV=prod
 PORT=4001
 DIR=$(pwd)
 
 if [ "$(ls -A $DIR)" ]; then
-  # Initial setup
-  mix deps.get --only prod
-  npm install
-  mix compile
-
-  # Compile assets
-  npm run deploy
-  mix phoenix.digest
-
-  # Custom tasks (like DB migrations)
-  mix ecto.migrate
-
-  # Finally run the server
-  #
-  exec mix phoenix.server
+  mix deps.get --only prod && \
+    npm install && \
+    mix compile && \
+    npm run deploy && \
+    mix phoenix.digest && \
+    mix ecto.create && \
+    mix ecto.migrate && \
+    exec mix phoenix.server
 else
   echo "$DIR is Empty"
-  exec bash
 fi
