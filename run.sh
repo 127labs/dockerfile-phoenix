425ae338f1090e15341c7fecabd25d4bfdb8c335
Imran Ismail <imran.codely@gmail.com>
2016-08-28 01:56:00 +0800

    Check if should run in dstributed mode

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 export MIX_ENV=prod
 export APP_DIR=$(pwd)
 
-if [ "$(ls -A $DIR)" ]; then
+if [[ "$(ls -A $DIR)" ]]; then
   mix clean && \
     mix deps.clean --all && \
     mix deps.get --only $MIX_ENV && \
@@ -13,7 +13,12 @@ if [ "$(ls -A $DIR)" ]; then
     mix phoenix.digest && \
     mix ecto.create && \
     mix ecto.migrate && \
-    exec mix phoenix.server
+
+    if [[ $NODE_NAME && $NODE_HOST ]]; then
+      exec elixir --name $NODE_NAME@$NODE_HOST -S mix phoenix.server
+    else
+      exec mix phoenix.server
+    fi
 else
   echo "$DIR is Empty"
 fi
