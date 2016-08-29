603d3ad1f98641a24448aa58639e8caacf1af115
Imran Ismail <imran.codely@gmail.com>
2016-08-29 20:21:59 +0800

    Update deployment stratergy

--- run.sh
+++ run.sh
@@ -1,25 +1,16 @@
 #!/bin/bash
 
-export MIX_ENV=prod
 export APP_DIR=$(pwd)
-export NODE_HOST=$(hostname -I)
+export APP_HOST=$(hostname -I)
 
-if [[ "$(ls -A $DIR)" ]]; then
-  mix clean && \
-    mix deps.clean --all && \
-    mix deps.get --only $MIX_ENV && \
-    npm install && \
-    mix compile && \
-    npm run deploy && \
-    mix phoenix.digest && \
-    mix ecto.create && \
-    mix ecto.migrate && \
+if [[ "$(ls -A $APP_DIR)" ]]; then
+  mix ecto.create && mix ecto.migrate
 
-    if [[ $COOKIE && $NODE_NAME ]]; then
-      exec elixir --name $NODE_NAME@$NODE_HOST --cookie $COOKIE -S mix phoenix.server
-    else
-      exec mix phoenix.server
-    fi
+  if [[ $APP_COOKIE && $APP_NAME ]]; then
+    exec elixir --name $APP_NAME@$APP_HOST --cookie $APP_COOKIE -S mix phoenix.server
+  else
+    exec mix phoenix.server
+  fi
 else
-  echo "$DIR is Empty"
+  echo "$APP_DIR is Empty"
 fi
